daa07af96260030c07e69a4561f4a753276c50e2
Rian de Oliveira <rian.oliveira.02@edu.unipar.br>
2022-01-20 10:05:43 -0300

    JavaScript Game

---
+++ script.js
@@ -0,0 +1,60 @@
+let player = {
+  name: 'Rian',
+  chips: 200,
+};
+let cards = [];
+let hasBlackJack = false;
+let isAlive = false;
+let message = '';
+let sum = 0;
+let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.querySelector('#cards-el');
+let playerEl = document.querySelector('#player-el');
+
+playerEl.textContent = player.name + ': $' + player.chips;
+function startGame() {
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  isAlive = true;
+  renderGame();
+}
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1;
+  if (randomCard > 10) {
+    return 10;
+  } else if (randomCard === 1) {
+    return 11;
+  }
+  return randomCard;
+}
+function renderGame() {
+  cardsEl.textContent = 'Cards: ';
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + ' ';
+  }
+  sumEl.textContent = `Sum: ${sum}`;
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card?';
+  } else if (sum === 21) {
+    message = "You've got a Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+
+    renderGame();
+  }
+}
